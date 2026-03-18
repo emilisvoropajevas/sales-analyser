036ebbfea6200f3fffdc5965dbc0ad827582c427
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-03-18 23:03:56 +0000

    fixed class Reports models with created_at field

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,7 +4,7 @@ from sqlmodel import Field, SQLModel
 class Reports(SQLModel, table = True):
     id : int | None = Field(default=None, primary_key=True)
     name : str
-    created_at = datetime = Field(default=datetime.now(tz=datetime.UTC))
+    created_at : datetime = Field(default=datetime.now(tz=datetime.UTC))
     date_range_start : str
     date_range_end : str
     data : str
